5b880b51d59c43a285bdebf74450c316ea96c56d
Cgotts <cobi.gottschalk@gmail.com>
2023-02-25 00:21:10 -0800

    Added some styling changes

--- react-portfolio/src/components/PortfolioContainer.js
+++ react-portfolio/src/components/PortfolioContainer.js
@@ -1,27 +1,29 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
-import Home from './pages/Home';
+import Resume from './pages/Resume';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState('About');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
-    }
     if (currentPage === 'About') {
       return <About />;
     }
     if (currentPage === 'Portfolio') {
       return <Portfolio />;
     }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
     return <Contact />;
   };
+
+  
 
   const handlePageChange = (page) => setCurrentPage(page);
 
