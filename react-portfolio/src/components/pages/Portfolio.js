e987183187ad87685c97f5530162bfd7914dd27b
Cgotts <cobi.gottschalk@gmail.com>
2023-03-22 16:53:46 -0700

    added gamer match to portfolio

--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import image13 from "./image13.png"
+
+
 export default function Portfolio() {
   return (
     <div>
@@ -24,7 +27,7 @@ export default function Portfolio() {
               <a href="https://cgotts1.github.io/API-Project/" target="_blank">
                 <img
                   src="https://cgotts1.github.io/V4-Portfolio/images/film-finder.jpg"
-                  alt="Image of work 1: 'Hand holding book'" id = "full-stack-work" className = "work" title = "Film Finder"
+                  alt=" Film Finder" id = "full-stack-work" className = "work" title = "Film Finder"
                 />
               </a>
             </article>
@@ -41,7 +44,7 @@ export default function Portfolio() {
               <a href="https://cgotts1.github.io/quote-generator/" target="_blank">
                 <img
                 src="https://cgotts1.github.io/V4-Portfolio/images/quote-generator.jpg"
-                alt="Image of work: 'Quote-Generator" id = "full-stack-work" className = "work" title = "Quote-Generator"
+                alt=" 'Quote-Generator" id = "full-stack-work" className = "work" title = "Quote-Generator"
                 />
 
               </a>
@@ -58,7 +61,7 @@ export default function Portfolio() {
               <a href="https://cgotts1.github.io/Weather-Dashboard/" target="_blank">
                 <img
                   src="https://cgotts1.github.io/V4-Portfolio/images/weather-dashboard.jpg"
-                  alt="Image of work: Weather-Dashboard" id = "full-stack-work" className = "work" title = "Weather-Dashboard"
+                  alt=" Weather-Dashboard" id = "full-stack-work" className = "work" title = "Weather-Dashboard"
                 />
 
               </a>
@@ -74,7 +77,7 @@ export default function Portfolio() {
               <a href="https://cgotts1.github.io/Work-Day-Scheduler/" target="_blank">
                 <img
                   src="https://cgotts1.github.io/V4-Portfolio/images/work-day-scheduler.jpg"
-                  alt="Image of work: Work-Day-Scheduler" id = "full-stack-work" className = "work" title = "Work-Day-Scheduler"
+                  alt=" Work-Day-Scheduler" id = "full-stack-work" className = "work" title = "Work-Day-Scheduler"
                 />
 
               </a>
@@ -90,7 +93,7 @@ export default function Portfolio() {
               <a href="https://cgotts1.github.io/Password-Generator/" target="_blank">
                 <img
                   src="https://cgotts1.github.io/V4-Portfolio/images/password-generator.jpg"
-                  alt="Image of work: Password-Generator" id = "full-stack-work" className = "work"  title = "Password-Generator"
+                  alt=" Password-Generator" id = "full-stack-work" className = "work"  title = "Password-Generator"
                 />
 
               </a>
@@ -106,7 +109,7 @@ export default function Portfolio() {
                 <a href="https://cgotts1.github.io/Anthropology-Quiz/" target="_blank">
                   <img
                     src="https://cgotts1.github.io/V4-Portfolio/images/anthropology-quiz.jpg"
-                    alt="Image of work: Anthropology-Quiz" id = "full-stack-work" className = "work"  title = "Anthropology-Quiz"
+                    alt=" Anthropology-Quiz" id = "full-stack-work" className = "work"  title = "Anthropology-Quiz"
                   />
   
                 </a>
@@ -122,7 +125,7 @@ export default function Portfolio() {
                 <a href="https://cgotts1.github.io/color-changer/" target="_blank">
                   <img
                     src="https://cgotts1.github.io/V4-Portfolio/images/color-changer.jpg"
-                    alt="Image of work: Color-Changer" id = "full-stack-work" className = "work"  title = "Color-Changer"
+                    alt=" Color-Changer" id = "full-stack-work" className = "work"  title = "Color-Changer"
                   />
   
                 </a>
@@ -138,7 +141,7 @@ export default function Portfolio() {
                 <a href="https://cgotts1.github.io/js-flash-cards/" target="_blank">
                   <img
                     src="https://cgotts1.github.io/V4-Portfolio/images/flash-cards.jpg"
-                    alt="Image of work: Flash-Cards" id = "full-stack-work" className = "work"  title = "Flash-Cards"
+                    alt=" Flash-Cards" id = "full-stack-work" className = "work"  title = "Flash-Cards"
                   />
   
                 </a>
@@ -154,7 +157,7 @@ export default function Portfolio() {
                 <a href="https://drive.google.com/file/d/1nTql6j8v9ov0MurceNV8lzc9ybklxvvY/view" target="_blank">
                   <img
                     src="https://github.com/Cgotts1/readme-generator/raw/main/image/nodequestions.png"
-                    alt="Image of work: Readme-Generator" id = "full-stack-work" className = "work"  title = "Readme-Generator"
+                    alt=" Readme-Generator" id = "full-stack-work" className = "work"  title = "Readme-Generator"
                   />
   
                 </a>
@@ -170,7 +173,7 @@ export default function Portfolio() {
                 <a href="https://drive.google.com/file/d/1h6JLU5_bIjOT5hWXo3d54XxKFUUBkfhP/view" target="_blank">
                   <img
                     src="https://github.com/Cgotts1/Team-Profile-Generator/raw/main/image/image1.png"
-                    alt="Image of work: Team-Profile-Generator" id = "full-stack-work" className = "work"  title = "Team-Profile-Generator"
+                    alt=" Team-Profile-Generator" id = "full-stack-work" className = "work"  title = "Team-Profile-Generator"
                   />
   
                 </a>
@@ -186,7 +189,23 @@ export default function Portfolio() {
                 <a href="https://protected-woodland-15218.herokuapp.com/" target="_blank">
                   <img
                     src="https://cgotts1.github.io/V4-Portfolio/images/fitness-planner.png"
-                    alt="Image of work: Fitness-Planner" id = "full-stack-work" className = "work"  title = "Fitness-Planner"
+                    alt="Fitness-Planner" id = "full-stack-work" className = "work"  title = "Fitness-Planner"
+                  />
+  
+                </a>
+              </article>
+
+              <article className="work2">
+                <p id = "full-stack-overlay" className ="overlay">Gamer Match</p>
+
+                <a href="https://github.com/Ryan-M-Taylor/Gamer-Match" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" className="bi bi-github overlay overlay-logo" viewBox="0 0 16 16" href="https://github.com/Cgotts1">
+  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+</svg></a>  
+  
+                <a href="https://peaceful-brook-99319.herokuapp.com/" target="_blank">
+                  <img
+                    src={image13}
+                    alt=" Gamer Work" id = "full-stack-work" className = "work"  title = "Gamer Match"
                   />
   
                 </a>
